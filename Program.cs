add05ba077e526cdfb9f2c1623d4b702356f7296
Nathan Harvey <Narvey@users.noreply.github.com>
2018-09-27 16:46:15 -0600

    https://stackoverflow.com/q/52541845/1739000
    files necessary to reproduce https://stackoverflow.com/q/52541845/1739000

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SORepro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var page = new { ClientQueryString = "thing" } as dynamic;
+
+            if (page != null && TryGetURLParams(page.ClientQueryString, out string stringOne))
+            {
+                stringOne.ToString();
+                return;
+            }
+        }
+
+        static bool TryGetURLParams(string input, out string stringOne)
+        {
+            stringOne = input.ToLower();//do real things instead.
+            return true;
+        }
+    }
+}
